9647f75297313c63aed5833831a5e38b3591776b
christian-art69 <christian.vidal2023@twk.cl>
2025-11-01 17:45:23 -0300

    Remove unused files and update dependencies; add jwt-decode package and configure HTTP client with interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,19 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-
 import { routes } from './app.routes';
+
+// 1. Importa el proveedor de HttpClient
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './core/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptorsFromDi())
+    
+    // 2. Añade el proveedor aquí
+    //    Usaremos un interceptor (te lo explico más abajo)
+    provideHttpClient(withInterceptors([
+      authInterceptor
+    ]))
   ]
 };
--- src/main.server.ts
+++
@@ -1,9 +0,0 @@
-
-import 'zone.js';
-import { bootstrapApplication, type BootstrapContext } from '@angular/platform-browser';
-import { AppComponent} from './app/app';
-import { appConfig} from './app/app.config';
-
-const bootstrap = (context?: BootstrapContext) => bootstrapApplication(AppComponent, appConfig, context);
-
-export default bootstrap;
