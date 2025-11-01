21b7f88cce20d083124af74dfb0a1fa5c2db8d6f
christian-art69 <christian.vidal2023@twk.cl>
2025-11-01 18:23:02 -0300

    Refactor application structure; remove unused files and components, and implement new components for guards, historial, interfaces, libros, prestamos, and usuarios

---
+++ src/app/interfaces/interfaces.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-interfaces',
+  imports: [],
+  templateUrl: './interfaces.html',
+  styleUrl: './interfaces.css'
+})
+export class Interfaces {
+
+}
